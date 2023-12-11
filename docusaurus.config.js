c987ab2d904bc72644370cef5eb66c7412c054e8
ThaylaalyahT <a81345@ualg.pt>
2023-12-11 17:11:55 +0000

    Docusaurus teste

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Site Demo',
-  tagline: 'O docsaurus fixe!',
+  tagline: 'O docsaurus é fixe!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
