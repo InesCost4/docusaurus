58f9d2bcf0fbbb3b3fd00487c9e60752aa11d61a
ThaylaalyahT <a81345@ualg.pt>
2023-12-18 15:12:19 +0000

    Novos nomes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Car workshop',
-  tagline: 'Oficina!',
+  tagline: 'Oficina de automóveis',
   favicon: 'img/favicon2.ico',
 
   // Set the production url of your site here
