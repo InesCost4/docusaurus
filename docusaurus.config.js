9f690bc3fb5fc5d9d3a91861ed70fe8d9c12ffe6
Ines <inessacosta@deloitte.pt>
2023-12-19 18:20:17 +0000

    ajustar os numero de posts

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,6 +47,7 @@ const config = {
         },
         blog: {
           showReadingTime: true,
+          blogSidebarCount: 6,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
